ba9ea24651d054adf2f5338ab5fcbc255c7f633c
g4ze <bhaiyajionline17@gmail.com>
2023-07-23 03:23:31 +0530

    did an update

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
+// const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+// tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+// tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+// tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+// tl.fromTo(".big-text", { opacity: 2 }, { opacity: 0, duration: 1, delay: 1 }, "-=1");
+// tl.fromTo("#myBtn", { opacity: 0 }, { opacity: 1, duration: 1 });
+// tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+
+// tl.fromTo(".message", { opacity: 0 }, { opacity: 1, duration: 1 });
+// /*for the timer here*/
+
+// tl.fromTo(".middle", { opacity: 0 }, { opacity: 1, duration: 1 });
+
+//=====================================
+
+
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo(".big-text", { opacity: 2 }, { opacity: 0, duration: 1, delay: 1 }, "-=1");
-tl.fromTo("#myBtn", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.to(".text", { y: "0%", duration: 1, stagger: 0 });
+tl.to(".slider", { y: "-100%", duration: 0, delay: 0 });
+tl.to(".intro", { y: "-100%", duration: 0 }, "-=1");
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 0, duration: 0, delay: 0 }, "-=1");
+tl.fromTo("#myBtn", { opacity: 0 }, { opacity: 0, duration: 0 });
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 0 });
 
-tl.fromTo(".message", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".message", { opacity: 0 }, { opacity: 1, duration: 0 });
 /*for the timer here*/
 
-tl.fromTo(".middle", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".middle", { opacity: 0 }, { opacity: 1, duration: 0 });
 
 
 
